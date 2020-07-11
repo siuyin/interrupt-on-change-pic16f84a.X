1a1e13f912adb6a0d73c720733b78c3a58287953
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-07-11 12:27:23 +0800

    Refactored main to removed wait and end of main loop.

--- main.c
+++ main.c
@@ -18,49 +18,20 @@
 
 #include <xc.h>
 
+#define _XTAL_FREQ 740000   // 6.8k and 100pF resistor is 10% and capacitor is 20% tolerance
 #define BUTTON RB4
 #define UPPER_LED RB2
 #define LOWER_LED RB3
-const unsigned char button_pushed_task_period_ticks = 2;
+
+// Global variables used by main and interrupt service routine.
 volatile unsigned char button_pushed_task_ctr;
-volatile unsigned char tick; // system timer tick
-const unsigned char tmr0_reload_val = 248;
+volatile unsigned char tick; // system timer tick is approx 11 ms.
 
-enum button_state_t {
-    pushed, maybeReleased, released
-};
-volatile enum button_state_t button_state;
-
-void __interrupt() interrupt_service_routine(void) {
-    if (T0IE && T0IF) { // timer 0 interrupt enable and interrupt flag
-        if (button_pushed_task_ctr > 0)button_pushed_task_ctr--;
-        tick++;
-
-        TMR0 = tmr0_reload_val; // reload TMR0 for next tick
-        T0IF = 0; // clear TMR0 interrupt flag
-    }
-
-    if (RBIE && RBIF) { // port b interrupt enable and interrupt request flag.
-        if (BUTTON == 0) {
-            LOWER_LED = ~LOWER_LED;
-            RBIE = 0; // disable port b change interrupt, will be re-enabled in main line code.
-        }
-        RBIF = 0; // The interrupt flag bit(s) must be cleared in software before re-enabling interrupts to avoid infinite interrupt requests.
-    }
-
-    if (EEIE && EEIF) { // eeprom write was completed
-
-        EEIF = 0;
-    }
-
-}
-
-#define _XTAL_FREQ 740000   // 6.8k and 100pF resitor is 10% and capacitor is 20% tolerance
+// Local function declarations.
 void setup_TMR0_for_interrupts(void);
 void setup_portb_for_interrupt_on_change(void);
 void setup_eeprom_write_interrupt(void);
 void check_button_pushed_and_toggle_LEDs(void);
-void wait_for_next_tick(unsigned char *);
 
 void main(void) {
     setup_TMR0_for_interrupts();
@@ -68,13 +39,15 @@ void main(void) {
     setup_eeprom_write_interrupt();
     ei();
 
-    unsigned char current_tick = 0;
     while (1) {
         check_button_pushed_and_toggle_LEDs();
-        wait_for_next_tick(&current_tick);
     }
     return;
 }
+
+
+
+const unsigned char tmr0_reload_val = 248;
 
 void setup_TMR0_for_interrupts(void) {
     tick = 0;
@@ -96,6 +69,8 @@ void setup_TMR0_for_interrupts(void) {
     LOWER_LED = 1;
 }
 
+
+
 void setup_portb_for_interrupt_on_change(void) {
     // Set port b to output, except for RB4 which is a button input.
     PORTB = 0;
@@ -103,9 +78,11 @@ void setup_portb_for_interrupt_on_change(void) {
     TRISB = 0;
     TRISB4 = 1; // RB4 is an input
 
-    button_state = released;
+    state = released;
     RBIE = 1;
 }
+
+
 
 unsigned char button_pushed_times;
 
@@ -114,43 +91,72 @@ void setup_eeprom_write_interrupt() {
     EEIE = 1; // eeprom interrupt enable
 }
 
+enum button_state_t {
+    pushed, maybeReleased, released
+};
+volatile enum button_state_t state;
+
 void check_button_pushed_and_toggle_LEDs(void) {
-    if (button_pushed_task_ctr != 0) {
+    static unsigned char lda; // lda: last done at
+    const unsigned char t = 2;
+
+    if (button_pushed_task_ctr != 0 || lda == tick) {
         return;
     }
 
-    switch (button_state) {
+    lda = tick;
+    button_pushed_task_ctr = t;
+
+    switch (state) {
         case released:
             if (BUTTON == 0) {
-                button_state = pushed;
+                state = pushed;
                 UPPER_LED = ~UPPER_LED;
-                eeprom_write(0,++button_pushed_times); // data can be read by the pickit2 programmer with: ./pk2cmd -ppic16f84a -ge0-4 -r -t
+                eeprom_write(0, ++button_pushed_times); // data can be read by the pickit2 programmer with: ./pk2cmd -ppic16f84a -ge0-4 -r -t
                 break;
             }
-            button_state = released;
+            state = released;
             break;
         case pushed:
             if (BUTTON == 0) {
-                button_state = pushed;
+                state = pushed;
                 break;
             }
-            button_state = maybeReleased;
+            state = maybeReleased;
             break;
         case maybeReleased:
             if (BUTTON == 0) {
-                button_state = pushed;
+                state = pushed;
                 break;
             }
-            button_state = released;
+            state = released;
             RBIE = 1; // enable port b change interrupt
             break;
     }
-
-    button_pushed_task_ctr = button_pushed_task_period_ticks;
 }
 
-void wait_for_next_tick(unsigned char * current_tick) {
-    while (*current_tick == tick) {
+
+
+// interrupt service routine
+
+void __interrupt() interrupt_service_routine(void) {
+    if (T0IE && T0IF) { // timer 0 interrupt enable and interrupt flag
+        if (button_pushed_task_ctr > 0)button_pushed_task_ctr--;
+        tick++;
+
+        TMR0 = tmr0_reload_val; // reload TMR0 for next tick
+        T0IF = 0; // clear TMR0 interrupt flag
     }
-    *current_tick = tick;
+
+    if (RBIE && RBIF) { // port b interrupt enable and interrupt request flag.
+        if (BUTTON == 0) {
+            LOWER_LED = ~LOWER_LED;
+            RBIE = 0; // disable port b change interrupt, will be re-enabled in main line code.
+        }
+        RBIF = 0; // The interrupt flag bit(s) must be cleared in software before re-enabling interrupts to avoid infinite interrupt requests.
+    }
+
+    if (EEIE && EEIF) { // eeprom write was completed
+        EEIF = 0;
+    }
 }
